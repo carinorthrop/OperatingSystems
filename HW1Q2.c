0799bd7a8c788329f76e10484fa2ab3622a62118
carinorthrop <50886022+carinorthrop@users.noreply.github.com>
2020-02-01 23:30:39 -0500

    Add files via upload

--- HW1Q2.c
+++ HW1Q2.c
@@ -47,7 +47,7 @@ void childprocess(const char * file_name)
             else
             {
                 //print the PID only once
-                printf("PID: %x \n", getpid());
+                printf("\n PID: %x \n", getpid());
                 wait(0); 
             }              
         }
