d7ecdfaba4cd50c81e4ce6a1756c772facb9f167
carinorthrop <50886022+carinorthrop@users.noreply.github.com>
2020-02-16 16:53:47 -0500

    Add files via upload

--- part3client.c
+++ part3client.c
@@ -15,39 +15,47 @@
 #include <fcntl.h>
 
 
-int main(int argc, char *argv[]) {
-    //Verify correct number of arguments 
-    if (argc != 2) {
-        printf("Please enter the correct number of arguments. Only need filename");
+int main(int argc, char *argv[]) 
+{
+    //parameter checking
+    if (argc != 2) 
+    {
+        printf("The correct parameters were not entered. Usage: file_name");
         exit(1);
     }
-    const int MAXLINE = 255;
-    char line[MAXLINE];
-    int input_fd;
+    const int MAX = 255;
+    char line[MAX];
+
+    //create the pipe
     char* pipe = "/tmp/part3";
+    mkfifo(pipe, 0666); 
 
-    //Create Named Pipe
-    mkfifo(pipe,0666); 
+    int input = open(pipe, O_WRONLY);
 
-    input_fd = open(pipe, O_WRONLY);
-
-    if (input_fd == -1) {
+    if (input == NULL)
+    {
         printf("Error opening file");
         exit(0);
     }
 
-    FILE *fp = fopen(argv[1], "r");
+    FILE *fd = fopen(argv[1], "r");
 
-    while(fgets(line, MAXLINE, fp) != NULL){
-        for (int i = 0; i < strlen(line); i++) {
+    //convert to uppercase
+    while(fgets(line, MAX, fd) != NULL)
+    {
+        for (int i = 0; i < strlen(line); i++) 
+        {
             line[i] = toupper(line[i]);
         }
-        write(input_fd,line,MAXLINE);
-        printf("%s",line);
+        write(input, line, MAX);
+        printf("%s", line);
     }
 
-    write(input_fd, "Stop\n", MAXLINE);
-    fclose(fp);
-    close(input_fd);
+    //send stop over to server
+    write(input, "Stop\n", MAX);
+
+    //close out pipe and file
+    fclose(fd);
+    close(input);
     return 0;
 }
