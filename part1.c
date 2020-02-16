054284945f86ec14c765271454fcc14f5e9113a1
carinorthrop <50886022+carinorthrop@users.noreply.github.com>
2020-02-16 13:07:59 -0500

    Add files via upload

--- part1.c
+++ part1.c
@@ -24,12 +24,9 @@ int main(int argc, char *argv[])
     }
     else
     {
-   int child = (int) fork();
-    //Verify we have a child 
-    if (child == -1){
-        printf("Error forking");
-        exit(0);
-    }else if (child == 0){
+        int child = (int) fork();
+        if (child == 0)
+        {
         //Open the file
         int fd = open(argv[2], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
         //stdout to the file
@@ -41,9 +38,7 @@ int main(int argc, char *argv[])
         //execute the file
         execl(argv[1],argv[1], (char*)NULL);
         exit(0);
-    }else{
-        wait(NULL);
-    }
+        }
     }
     return 0;
 }
