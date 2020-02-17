7ba7df12e0f11fe53a5e109c21449fb5e81e43b0
carinorthrop <50886022+carinorthrop@users.noreply.github.com>
2020-02-17 14:53:26 -0500

    Add files via upload

--- part5server.c
+++ part5server.c
@@ -16,26 +16,23 @@
 #include <sys/types.h>
 #include <sys/mman.h>
 
-int main(int argc, char * argv[]) 
-{
+int main(int argc, char * argv[]) {
     const int MAXLINE = 1024;
     char line[MAXLINE];
     int input_fd;
     int * tmp;
     char * msg;
 
-    //open file 
-    if ((input_fd = open("middleman", O_RDWR | O_CREAT)) < 0)
-    {
-        printf("There was a problm opening the file\n");
+    // open input file
+    if ((input_fd = open("middleman", O_RDWR | O_CREAT)) < 0){
+        printf("Unnable to open file\n");
         exit(1);
     }
 
     ftruncate(input_fd, MAXLINE);
 
-    //use mmap on input 
-    if ((tmp = (int*) mmap(0, MAXLINE, PROT_READ, MAP_SHARED, input_fd, 0)) == MAP_FAILED)
-    {
+    // mmap input
+    if ((tmp = (int*) mmap(0, MAXLINE, PROT_READ, MAP_SHARED, input_fd, 0)) == MAP_FAILED){
         printf("Error with mmap input\n");
         exit(1);
     }
@@ -44,19 +41,14 @@ int main(int argc, char * argv[])
 
     // read
     int n = -1; // ensure a change first time through loop
-    while (1) 
-    {
-        if (n == tmp[0]) 
-        {
+    while (1) {
+        if (n == tmp[0]) {
             continue;
         }
         n = tmp[0];
         if (strcmp(msg, "STOP") == 0)
-        {
             break;
-        }
         printf("%s", msg);
     }
     unlink("middleman");
-    return 0;
 }
