d2327dec7063453c5e05992a9bd45af9ccf45994
carinorthrop <50886022+carinorthrop@users.noreply.github.com>
2020-02-19 13:31:41 -0500

    Add files via upload

--- part5client.c
+++ part5client.c
@@ -1,111 +1,72 @@
-
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <string.h>
-#include <sys/mman.h>
 #include <ctype.h>
+#include <sys/mman.h>
 
-int main(int argc, char * argv[]) 
-{
-    //parameter checking 
-    if (argc != 2)
-     {
-        printf("Please enter the correct number of arguments. Only need filename");
-        exit(1);
-    }
+// Helper function to convert a string to upper case.
+//   Loops through the characters of a string and makes them uppercase
+void makeupper(char* str) {
+	while (*str) {
+		*str = toupper((unsigned char) *str);
+		str++;
+	}
+}
 
-    const int MAXLINE = 1024;
-    char line[MAXLINE];
-    int output_fd;
-    int * tmp;
-    char * msg;
-    const char FILE_SIZE = sizeof(char) + sizeof(int);
-
-    // open input file
-    FILE *fp = fopen(argv[1], "r"); 
-    if (fp == NULL) 
-    {
-        printf("File not found.");
-        exit(1);
-    }
-    // open/create output file
-    if ((output_fd = open("middleman", O_RDWR | O_CREAT | O_TRUNC, 0644)) < 0)
-    {
-        printf("Unable to open file\n");
-        exit(1);
-    }
-
-	//file_size
-    int empty;
-	if ((empty = lseek(output_fd, FILE_SIZE - 1, SEEK_SET)) == -1) 
-    {
-		perror("Problem with the size");
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		printf("Usage: %s file-to-read\n", argv[0]);
 		exit(1);
 	}
 
-	//write and empty bit 
-	if ((empty = write(output_fd, "", 1)) != 1) 
-    {
-		perror("Problem with writing an empty bit");
+	const int FILESIZE = sizeof(char) + sizeof(int);
+	const char FILENAME[] = "temp.bin";
+
+	// Open a temporary file
+	int fd = open(FILENAME, O_RDWR|O_CREAT|O_TRUNC, 0600);
+	if (fd == -1) {
+		perror("open");
 		exit(1);
 	}
-          
-    ftruncate(output_fd, MAXLINE);
 
-    // mmap output
-    if ((tmp = (int*) mmap(0, MAXLINE, PROT_READ | PROT_WRITE, MAP_SHARED, output_fd, 0)) == MAP_FAILED)
-    {
-        printf("Error with mmap output\n");
-        exit(1);
-    }
-       
-    msg = (char*) tmp + 4;
+	// Open the map!
+	int* count = (int *)mmap(NULL, FILESIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0); 
+	if (count == MAP_FAILED) {
+		perror("mmap");
+		exit(1);
+	}
 
-    //reading in
-    
+	// Set a reference to the pointer for the string
+	char* str = (char *)count + sizeof(int);
+
+	FILE* sourceFile = fopen(argv[1], "r");
+	if (sourceFile == NULL) {
+		perror(argv[1]);
+		exit(1);
+	}
+
+	// Begin reading the lines from the source file
 	char* line;
 	size_t len = 0;
 
-    char* str = (char *)tmp + sizeof(int);
-
-	while (getline(&line, &len, fp) != -1) 
-    {
-		for (int i = 0; i < strlen(line); i++) 
-        {
-            line[i] = toupper(line[i]);
-        }
+	while (getline(&line, &len, sourceFile) != -1) {
+		makeupper(line); // convert to uppercase
 		strcpy(str, line); //move the line to the memory for the string
-		++*tmp; // update the count
+		(*count)++; // update the count
 
 		sleep(1);
 	}
 
+	// Send signal to the server to die
+	strcpy(str, "Stop\n");
+	(*count)++;
 
-
-
-
-
-
-    /*while(fgets(line, MAXLINE, fp) != NULL) 
-    {
-        for (int i = 0; i < strlen(line); i++) 
-        {
-            line[i] = toupper(line[i]);
-        }
-        // write to memory
-        strncpy(msg, line, MAXLINE);
-        puts(msg);
-        ++*tmp;
-        sleep(1); // pauses for one second
-    }*/
-
-    strncpy(msg, "STOP", 5);
-    puts("STOP");
-    
-
-
+	// Detach segments and close files
+	munmap(count, FILESIZE);
+	close(fd);
+	fclose(sourceFile);
 }
