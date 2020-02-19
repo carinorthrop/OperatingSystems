c096b6dd5c9cf7c0f5121e79cf2b336ab4d606c5
carinorthrop <50886022+carinorthrop@users.noreply.github.com>
2020-02-18 20:31:22 -0500

    Add files via upload

--- part5server.c
+++ part5server.c
@@ -7,40 +7,58 @@
 #include <fcntl.h> 
 #include <string.h>
 #include <sys/mman.h>
+#include <ctype.h>
 
 int main(int argc, char * argv[]) {
+    //Verify correct number of arguments 
+    if (argc != 2) {
+        printf("Please enter the correct number of arguments. Only need filename");
+        exit(1);
+    }
     const int MAXLINE = 1024;
     char line[MAXLINE];
-    int input_fd;
+    int output_fd;
     int * tmp;
     char * msg;
 
     // open input file
-    if ((input_fd = open("middleman", O_RDWR | O_CREAT)) < 0){
-        printf("Unnable to open file\n");
+    FILE *fp = fopen(argv[1], "r"); 
+    if (fp == NULL) {
+        printf("File not found.");
         exit(1);
     }
-
-    ftruncate(input_fd, MAXLINE);
-
-    // mmap input
-    if ((tmp = (int*) mmap(0, MAXLINE, PROT_READ, MAP_SHARED, input_fd, 0)) == MAP_FAILED){
-        printf("Error with mmap input\n");
+    // open/create output file
+    if ((output_fd = open("middleman", O_RDWR | O_CREAT | O_TRUNC, 0644)) < 0){
+        printf("Unable to open file\n");
         exit(1);
     }
+          
+    ftruncate(output_fd, MAXLINE);
 
+    // mmap output
+    if ((tmp = (int*) mmap(0, MAXLINE, PROT_READ | PROT_WRITE, MAP_SHARED, output_fd, 0)) == MAP_FAILED){
+        printf("Error with mmap output\n");
+        exit(1);
+    }
+       
     msg = (char*) tmp + 4;
 
-    // read
-    int n = -1; // ensure a change first time through loop
-    while (1) {
-        if (n == tmp[0]) {
-            continue;
+    // mmap input
+    while(fgets(line, MAXLINE, fp) != NULL) {
+        for (int i = 0; i < strlen(line); i++) {
+            line[i] = toupper(line[i]);
         }
-        n = tmp[0];
-        if (strcmp(msg, "STOP") == 0)
-            break;
-        printf("%s", msg);
+        // write to memory
+        strncpy(msg, line, MAXLINE);
+        puts(msg);
+        ++*tmp;
+        sleep(1); // pauses for one second
     }
-    unlink("middleman");
+
+    strncpy(msg, "STOP", 5);
+    ++*tmp;
+    puts("STOP");
+    
+
+
 }
