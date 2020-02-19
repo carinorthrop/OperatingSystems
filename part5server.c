97baac5ab728586dd36e5ebb72d2766141575838
carinorthrop <50886022+carinorthrop@users.noreply.github.com>
2020-02-19 12:52:10 -0500

    Add files via upload

--- part5server.c
+++ part5server.c
@@ -8,7 +8,8 @@
 #include <string.h>
 #include <sys/mman.h>
 
-int main(int argc, char * argv[]) {
+int main(int argc, char * argv[]) 
+{
     const int MAXLINE = 1024;
     char line[MAXLINE];
     int input_fd;
@@ -16,30 +17,50 @@ int main(int argc, char * argv[]) {
     char * msg;
 
     // open input file
-    if ((input_fd = open("middleman", O_RDWR | O_CREAT)) < 0){
+    if ((input_fd = open("middleman", O_RDWR | O_CREAT)) < 0)
+    {
         printf("Unnable to open file\n");
         exit(1);
     }
 
-    ftruncate(input_fd, MAXLINE);
-
     // mmap input
-    if ((tmp = (int*) mmap(0, MAXLINE, PROT_READ, MAP_SHARED, input_fd, 0)) == MAP_FAILED){
+    if ((tmp = (int*) mmap(0, MAXLINE, PROT_READ, MAP_SHARED, input_fd, 0)) == MAP_FAILED)
+     {
         printf("Error with mmap input\n");
         exit(1);
     }
 
+    const char FILE_SIZE = sizeof(char) + sizeof(int);
+	//file_size
+    int empty;
+	if ((empty = lseek(tmp, FILE_SIZE - 1, SEEK_SET)) == -1) 
+    {
+		perror("lseek");
+		exit(1);
+	}
+
+	//write and empty bit 
+	if ((empty = write(tmp, "", 1)) != 1) 
+    {
+		perror("write");
+		exit(1);
+	}
+
     msg = (char*) tmp + 4;
 
-    // read
+    // read in the input 
     int n = -1; // ensure a change first time through loop
-    while (1) {
-        if (n == tmp[0]) {
+    while (1)
+     {
+        if (n == tmp[0]) 
+        {
             continue;
         }
         n = tmp[0];
         if (strcmp(msg, "STOP") == 0)
+        {
             break;
+        }
         printf("%s", msg);
     }
     unlink("middleman");
