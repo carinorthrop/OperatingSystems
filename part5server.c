4f4041fd747c4ddc7875a40edaab17308681a401
carinorthrop <50886022+carinorthrop@users.noreply.github.com>
2020-02-18 20:16:45 -0500

    Add files via upload

--- part5server.c
+++ part5server.c
@@ -9,7 +9,7 @@
 
 int main() {
 	const int FILESIZE = sizeof(char) + sizeof(int);
-	const char FILENAME[] = "testfile.txt";
+	const char FILENAME[] = "temp.bin";
 
 	// Open a temporary file
 	int fd = open(FILENAME, O_RDWR|O_CREAT|O_TRUNC, 0600);
