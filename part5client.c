d6f887e990ef421e5c35e8c2022b85f1685f95d3
carinorthrop <50886022+carinorthrop@users.noreply.github.com>
2020-02-18 19:54:43 -0500

    Add files via upload

--- part5client.c
+++ part5client.c
@@ -28,20 +28,18 @@ int main(int argc, char * argv[])
     const int MAXLINE = 1024;
     int output_fd;
     int * tmp;
-    char * msg;
+    const int FILE_SIZE = sizeof(char) + sizeof(int);
     const char FILE_NAME[] = "testfile.txt";
 
     // open the file
-    if ((output_fd = open(FILE_NAME, O_RDWR | O_CREAT | O_TRUNC, 0644)) < 0)
+    if ((output_fd = open(FILE_NAME, O_RDWR | O_CREAT | O_TRUNC, 0644)) != -1)
     {
         printf("There was a problem opening this file.\n");
         exit(1);
     }
-          
-    ftruncate(output_fd, MAXLINE);
 
     //open the map  
-    if ((tmp = (int*) mmap(0, MAXLINE, PROT_READ | PROT_WRITE, MAP_SHARED, output_fd, 0)) == MAP_FAILED){
+    if ((tmp = (int*) mmap(NULL, FILE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, output_fd, 0)) == MAP_FAILED){
         printf("There was a problem opening the map.\n");
         exit(1);
     }
@@ -52,11 +50,8 @@ int main(int argc, char * argv[])
         printf("File not found.");
         exit(1);
     }
-       
-    //msg = (char*) tmp + 4;
 
-	char* str = (char *)msg + sizeof(int);
-	*msg = 0;
+	char* str = (char *)tmp + sizeof(int);
 	char* line;
 	size_t len = 0;
     
@@ -73,9 +68,13 @@ int main(int argc, char * argv[])
         sleep(1); // pauses for one second
     }
 
-    strncpy(str, "Stop\n", 5);
+    strncpy(str, "Stop\n");
     ++*tmp;
-    puts("STOP");
+    
+    
+    munmap(tmp, FILE_SIZE);
+	close(output_fd);
+	fclose(fp);
     
 
 
