f613313e2df90d563ab249e4c19bef7489b1c28f
carinorthrop <50886022+carinorthrop@users.noreply.github.com>
2020-02-19 15:07:25 -0500

    Add files via upload

--- part5server.c
+++ part5server.c
@@ -1,66 +1,48 @@
-// Caroline Northrop 
-// Part 5 server
-// Due Febuary 24th
-// OS 4029
+//Ashley Wilhelm 
+//Homework 2: Part 5 Server
 
-
-#include <errno.h>
-#include <ctype.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <sys/shm.h>
-#include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h> 
+#include <string.h>
+#include <sys/mman.h>
 
-int main(int argc, char * argv[]) 
-{
+int main(int argc, char * argv[]) {
     const int MAXLINE = 1024;
     char line[MAXLINE];
     int input_fd;
     int * tmp;
     char * msg;
 
-    //open file
-    if ((input_fd = open("test", O_RDWR | O_CREAT)) < 0){
-        printf("There was a problem opening the file\n");
+    // open input file
+    if ((input_fd = open("middleman", O_RDWR | O_CREAT)) < 0){
+        printf("Unnable to open file\n");
         exit(1);
     }
 
     ftruncate(input_fd, MAXLINE);
 
-    //mmap
-    if ((tmp = (int*) mmap(0, MAXLINE, PROT_READ, MAP_SHARED, input_fd, 0)) == MAP_FAILED)
-    {
-        printf("There was a problem with the mmap input\n");
+    // mmap input
+    if ((tmp = (int*) mmap(0, MAXLINE, PROT_READ, MAP_SHARED, input_fd, 0)) == MAP_FAILED){
+        printf("Error with mmap input\n");
         exit(1);
     }
 
     msg = (char*) tmp + 4;
 
-    // read in input 
-    int n = -1; 
-
-    while (1)
-    {
-        if (n == tmp[0]) 
-        {
+    // read
+    int n = -1; // ensure a change first time through loop
+    while (1) {
+        if (n == tmp[0]) {
             continue;
         }
         n = tmp[0];
-
-        //checking for a stop from the client 
-        if (strcmp(msg, "Stop\n") == 0)
-        {
+        if (strcmp(msg, "STOP") == 0)
             break;
-        }
-
         printf("%s", msg);
     }
-
-    unlink("test");
-    return 0;
+    unlink("middleman");
 }
