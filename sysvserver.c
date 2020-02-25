3e8e4ce1fe3de7ec15adf73f41e15961f9cf21d9
carinorthrop <50886022+carinorthrop@users.noreply.github.com>
2020-02-24 21:08:12 -0500

    Add files via upload

---
+++ sysvserver.c
@@ -0,0 +1,17 @@
+// Caroline Northrop
+// Homework 3 sysv server
+// Due Febuary 28th
+// OS 4029
+
+#include <errno.h>
+#include <ctype.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/mman.h>
