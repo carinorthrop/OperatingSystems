ce2f614d31c7f5b5168c05c027ad242969df1662
carinorthrop <50886022+carinorthrop@users.noreply.github.com>
2020-02-26 13:42:39 -0500

    Add files via upload

--- raceclient.c
+++ raceclient.c
@@ -15,52 +15,64 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/mman.h>
-int main(int argc, char * argv[]) {
-    //Verify correct number of arguments
-    if (argc != 2) {
+
+int main(int argc, char * argv[]) 
+{
+    //parameter checking 
+    if (argc != 2) 
+    {
         printf("Please enter the correct number of arguments. Only need filename");
         exit(1);
     }
+
     const int MAXLINE = 1024;
     char line[MAXLINE];
     int output_fd;
     int * tmp;
     char * msg;
 
-    // open input file
+    //open file 
     FILE *fp = fopen(argv[1], "r");
-    if (fp == NULL) {
-        printf("File not found.");
+    if (fp == NULL) 
+    {
+        printf("The file was not found.");
         exit(1);
     }
-    // open/create output file
-    if ((output_fd = open("middleman", O_RDWR | O_CREAT | O_TRUNC, 0644)) < 0){
-        printf("Unable to open file\n");
+
+    //open output file
+    if ((output_fd = open("middleman", O_RDWR | O_CREAT | O_TRUNC, 0644)) < 0)
+    {
+        printf("There was a problem openin this file");
         exit(1);
     }
 
     ftruncate(output_fd, MAXLINE);
 
-    // mmap output
-    if ((tmp = (int*) mmap(0, MAXLINE, PROT_READ | PROT_WRITE, MAP_SHARED, output_fd, 0)) == MAP_FAILED){
-        printf("Error with mmap output\n");
+    //mmap
+    if ((tmp = (int*) mmap(0, MAXLINE, PROT_READ | PROT_WRITE, MAP_SHARED, output_fd, 0)) == MAP_FAILED)
+    {
+        printf("There was a problem with mmap");
         exit(1);
     }
 
     msg = (char*) tmp + 4;
 
-    // mmap input
-    while(fgets(line, MAXLINE, fp) != NULL) {
-        for (int i = 0; i < strlen(line); i++) {
+    //mmap input
+    while(fgets(line, MAXLINE, fp) != NULL) 
+    {
+        //conver to upper case
+        for (int i = 0; i < strlen(line); i++) 
+        {
             line[i] = toupper(line[i]);
         }
         // write to memory
         strncpy(msg, line, MAXLINE);
         puts(msg);
         (*tmp)++;
-        sleep(1); // pauses for one second
+        sleep(1); 
     }
 
     strncpy(msg, "STOP", 5);
     (*tmp)++;
-    //puts("STOP");
+    puts("STOP");
+}
--- raceserver.c
+++ raceserver.c
@@ -16,7 +16,8 @@
 #include <sys/types.h>
 #include <sys/mman.h>
 
-int main(int argc, char * argv[]) {
+int main(int argc, char * argv[]) 
+{
     const int MAXLINE = 1024;
     char line[MAXLINE];
     int input_fd;
@@ -24,25 +25,29 @@ int main(int argc, char * argv[]) {
     char * msg;
 
     // open input file
-    if ((input_fd = open("middleman", O_RDWR | O_CREAT)) < 0){
-        printf("Unnable to open file\n");
+    if ((input_fd = open("middleman", O_RDWR | O_CREAT)) < 0)
+    {
+        printf("There was a problem opening the file.");
         exit(1);
     }
 
     ftruncate(input_fd, MAXLINE);
 
     // mmap input
-    if ((tmp = (int*) mmap(0, MAXLINE, PROT_READ, MAP_SHARED, input_fd, 0)) == MAP_FAILED){
-        printf("Error with mmap input\n");
+    if ((tmp = (int*) mmap(0, MAXLINE, PROT_READ, MAP_SHARED, input_fd, 0)) == MAP_FAILED)
+    {
+        printf("There was a mmap problem");
         exit(1);
     }
 
     msg = (char*) tmp + 4;
 
-    // read
+    //read in the input file 
     int n = -1; // ensure a change first time through loop
-    while (1) {
-        if (n == tmp[0]) {
+    while (1) 
+    {
+        if (n == tmp[0]) 
+        {
             continue;
         }
         n = tmp[0];
