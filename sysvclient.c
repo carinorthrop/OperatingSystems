3a74d9c5a35485183dbb995dc5991127e976f907
carinorthrop <50886022+carinorthrop@users.noreply.github.com>
2020-02-26 14:38:45 -0500

    Add files via upload

--- sysvclient.c
+++ sysvclient.c
@@ -7,74 +7,88 @@
 #include <ctype.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <sys/shm.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
+#include <sys/shm.h>
 #include <sys/types.h>
-#include <sys/mman.h>
 
-void upper_string(char []);
-const int SHM_SIZE = 1024;  /* make it a 1K shared memory segment */
-const char FILE_NAME[] = "hwtest.txt";
+const int SHM_SIZE = 1024;
+const char FILE_NAME[] = "txt.txt";
 
-
-int main(int argc, char *argv[])
+int main(int argc, char* argv[]) 
 {
-    key_t key;
-    int shmid;
-    char *data;
-    int n;
-    const int MAXLINE = 255;
-    char line[MAXLINE];
+    //parameter checking
+    if (argc != 2) 
+    {
+		printf("The correct parameters were not entered. Usage: file_name");
+		exit(1);
+	}
 
-    /* make the key: */
-    if ((key = ftok(FILE_NAME, 1)) == -1) {
-        perror("ftok");
-        exit(1);
-    }
+	//create a key
+	key_t key; 
+	if ((key =ftok(FILE_NAME, 1)) == -1) 
+    {
+		perror("ftok");
+		exit(1);
+	}
 
-    /* connect to (and possibly create) the segment: */
-    if ((shmid = shmget(key, SHM_SIZE, 0644 | IPC_CREAT)) == -1) {
-        perror("shmget");
-        exit(1);
-    }
-     /* attach to the segment to get a pointer to it: */
-    data = shmat(shmid, (void *)0, 0);
-    if (data == (char *)(-1)) {
-        perror("shmat");
-        exit(1);
-    }
+	//create memory space
+	int shmid;
+	if ((shmid = shmget(key, SHM_SIZE, 0644|IPC_CREAT)) == -1) 
+    {
+		perror("shmget");
+		exit(1);
+	}
 
-    /* modify the segment, based on the command line: */
-    FILE *fp;
-    fp = fopen(argv[1],"r");
+	//attach
+	char* data = shmat(shmid, (void *)0, 0);
+	if (data == (char *)(-1)) 
+    {
+		perror("shmat");
+		exit(1);
+	}
 
+	//open
+	FILE* file = fopen(argv[1], "r");
+	if (file == NULL) 
+    {
+		perror(argv[1]);
+		exit(1);
+	}
 
-    strncpy(data, fgets(line,MAXLINE,fp), SHM_SIZE);
-    sleep(5);
+	char* str = (char *)data + sizeof(int);
+	*data = 0;
+	char* line;
+	size_t length = 0;
 
-    upper_string(line);
-    strncpy(data, line, SHM_SIZE);
-    sleep(5);
+    //read in input
+	while (getline(&line, &length, file) != -1) 
+    {
+        //convert to uppercase
+		for (int i = 0; i < strlen(line); i++) 
+        {
+            line[i] = toupper(line[i]);
+        }
 
-    /* detach from the segment: */
-    if (shmdt(data) == -1) {
+		strcpy(str, line);
+		(*data)++; 
+		sleep(1);
+	}
+
+	//tell server to break out of loop
+	strcpy(str, "Stop\n");
+	(*data)++;
+
+	//detach 
+	if (shmdt(data) == -1) 
+	{
         perror("shmdt");
         exit(1);
     }
-    return 0;
-}
 
-void upper_string(char s[])
-{
-				int c=0;
-				while (s[c] != '\0'){
-					if (s[c] >= 'a' && s[c] <= 'z'){
-						s[c] = s[c] - 32;
-						}
-					c++;
-					}
+	//delete
+	shmdt(str);
 }
--- sysvserver.c
+++ sysvserver.c
@@ -7,60 +7,72 @@
 #include <ctype.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <sys/shm.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
+#include <sys/shm.h>
 #include <sys/types.h>
-#include <sys/mman.h>
+
 
 const int SHM_SIZE = 1024;
-const char FILE_NAME[] = "hwtest.txt";
+const char FILE_NAME[] = "txt.txt";
 
-int main(int argc, char *argv[])
+
+int main() 
 {
-    key_t key;
-    int shmid;
-    char *data;
-    int n;
-
-    /* make the key: */
-    if ((key = ftok(FILE_NAME, 1)) == -1) {
-        perror("ftok");
-        exit(1);
-    }
-
-    /* connect to (and possibly create) the segment: */
-    if ((shmid = shmget(key, SHM_SIZE, 0644 | IPC_CREAT)) == -1)
+	//create a key
+	key_t key;
+	if ((key = ftok(FILE_NAME, 1)) == -1) 
     {
-        perror("shmget");
-        exit(1);
-    }
-        /* attach to the segment to get a pointer to it: */
-    data = shmat(shmid, (void *)0, 0);
-    if (data == (char *)(-1)) {
-        perror("shmat");
-        exit(1);
-    }
+		perror("ftok");
+		exit(1);
+	}
 
-    /* read the segment */
-    n = 0;
-    while ( (n < 10))
+	//create memory space
+	int shmid;
+	if ((shmid = shmget(key, SHM_SIZE, 0644|IPC_CREAT)) == -1) 
     {
-        printf("%s\"\n", data);
-        sleep(5);   /* Sleep 5 seconds) */
-        n++;
-    }
+		perror("shmget");
+		exit(1);
+	}
 
-    //detach from
-    if (shmdt(data) == -1) {
+	//attach
+	char* data = shmat(shmid, (void *)0, 0);
+	if (data == (char *)(-1)) 
+    {
+		perror("shmat");
+		exit(1);
+	}
+
+	char* str = (char *)data + sizeof(int);
+
+	//read in input 
+	int old = *data;
+	while (1) 
+    {
+		
+		if (old != *data) 
+        {
+			old = *data; 
+			printf("%s", str);
+
+			//listening for stop from client 
+			if (strcmp(str, "Stop\n") == 0) 
+            {
+				break;
+			}
+		}
+	}
+
+	//detach 
+    if (shmdt(data) == -1) 
+	{
         perror("shmdt");
         exit(1);
     }
 
-    /* delete the IPC structure */
-    shmctl(shmid, IPC_RMID, NULL);
-    return 0;
+	//delete
+	shmctl(shmid, IPC_RMID, NULL);
 }
