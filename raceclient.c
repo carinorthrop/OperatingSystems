4d38421d80bd76b1e801d8001f96e4e8bb90b486
carinorthrop <50886022+carinorthrop@users.noreply.github.com>
2020-02-26 13:58:08 -0500

    Add files via upload

--- raceclient.c
+++ raceclient.c
@@ -16,63 +16,80 @@
 #include <sys/types.h>
 #include <sys/mman.h>
 
-int main(int argc, char * argv[]) 
+const int SHM_SIZE = 1024;
+const char FILE_NAME[] = "txt.txt";
+
+int main(int argc, char* argv[]) 
 {
-    //parameter checking 
+    //parameter checking
     if (argc != 2) 
     {
-        printf("Please enter the correct number of arguments. Only need filename");
-        exit(1);
-    }
+		printf("The correct parameters were not entered. Usage: file_name");
+		exit(1);
+	}
 
-    const int MAXLINE = 1024;
-    char line[MAXLINE];
-    int output_fd;
-    int * tmp;
-    char * msg;
-
-    //open file 
-    FILE *fp = fopen(argv[1], "r");
-    if (fp == NULL) 
+	//create a key
+	key_t key; 
+	if ((key =ftok(FILE_NAME, 1)) == -1) 
     {
-        printf("The file was not found.");
-        exit(1);
-    }
+		perror("ftok");
+		exit(1);
+	}
 
-    //open output file
-    if ((output_fd = open("middleman", O_RDWR | O_CREAT | O_TRUNC, 0644)) < 0)
+	//create memory space
+	int shmid;
+	if ((shmid = shmget(key, SHM_SIZE, 0644|IPC_CREAT)) == -1) 
     {
-        printf("There was a problem openin this file");
-        exit(1);
-    }
+		perror("shmget");
+		exit(1);
+	}
 
-    ftruncate(output_fd, MAXLINE);
-
-    //mmap
-    if ((tmp = (int*) mmap(0, MAXLINE, PROT_READ | PROT_WRITE, MAP_SHARED, output_fd, 0)) == MAP_FAILED)
+	//attach
+	char* data = shmat(shmid, (void *)0, 0);
+	if (data == (char *)(-1)) 
     {
-        printf("There was a problem with mmap");
-        exit(1);
-    }
+		perror("shmat");
+		exit(1);
+	}
 
-    msg = (char*) tmp + 4;
-
-    //mmap input
-    while(fgets(line, MAXLINE, fp) != NULL) 
+	//open
+	FILE* file = fopen(argv[1], "r");
+	if (file == NULL) 
     {
-        //conver to upper case
-        for (int i = 0; i < strlen(line); i++) 
+		perror(argv[1]);
+		exit(1);
+	}
+
+	char* str = (char *)data + sizeof(int);
+	*data = 0;
+	char* line;
+	size_t length = 0;
+
+    //read in input
+	while (getline(&line, &length, file) != -1) 
+    {
+        //convert to uppercase
+		for (int i = 0; i < strlen(line); i++) 
         {
             line[i] = toupper(line[i]);
         }
-        // write to memory
-        strncpy(msg, line, MAXLINE);
-        puts(msg);
-        (*tmp)++;
-        sleep(1); 
+
+		strcpy(str, line);
+		(*data)++; 
+		sleep(1);
+	}
+
+	//tell server to break out of loop
+	strcpy(str, "Stop\n");
+	(*data)++;
+
+	//detach 
+	if (shmdt(data) == -1) 
+	{
+        perror("shmdt");
+        exit(1);
     }
 
-    strncpy(msg, "STOP", 5);
-    (*tmp)++;
-    puts("STOP");
+	//delete
+	shmdt(str);
 }
